abd255afe63a5bcfe3fc059308b2d6403f98f872
Ravshan <rv_76@mail.ru>
2020-01-17 18:16:47 +0500

    Add files via upload

---
+++ main2.php
@@ -0,0 +1,103 @@
+﻿<?php 
+/**
+* Базовый класс для животных
+*/
+abstract class Animal
+{
+	// уникальный идентификатор животного
+	static $id=0;
+
+	// получения продукта, то есть сколько даёт... 
+	public abstract function getProduct();
+
+}
+
+ /**
+ * Класс Коровы
+ */
+class Cow extends Animal
+{
+	// конструктор для уникального идентификатор	 
+	public function __construct()
+	{
+		$this->id = parent::$id++;
+	}
+	
+	//  молоко 
+	public function getProduct()
+	{
+		return rand(8,10);
+	} 
+	
+} 
+
+/**
+ * Класс курицы
+ */
+
+class Hen extends Animal
+{
+ 	// конструктор для уникального идентификатор
+	public function __construct()
+	{
+		$this->id = parent::$id++;
+	}
+	
+	//яицо
+	public function getProduct()
+	{
+		return rand(0,1);
+	} 
+}
+// Хлев
+class Barn 
+{
+	//регистрация коров в хлеву
+	public function createCow()
+	{
+		return new Cow;
+	}
+
+	//регистрация куриц в хлеву
+	public function createHen()
+	{
+		return new Hen;
+	}
+} 
+
+// Создание объекта Хлев
+$barn = new Barn();
+
+$array = array(); // массив для хранения 
+
+//регистрация кур
+for($i=1;$i<=20;$i++){
+    $array[]=$barn->createHen();
+}
+
+//регистрация коров
+for($i=1;$i<=10;$i++){
+    $array[]=$barn->createCow();
+}
+ 
+// корзина для молоко и яиц 
+$milk = 0;
+$eggs = 0; 
+
+// процесс собрание продуктов
+foreach ($array as $animals){
+
+	 if ($animals instanceOf Cow)
+	 {
+	 	$milk += $animals->getProduct();
+	 }
+	 elseif ($animals instanceOf Hen) {
+	 	 $eggs += $animals->getProduct();
+	 }
+
+}
+//результат
+echo "eggs =  ".$eggs.'</br>';
+echo "milk = ".$milk;
+
+?> 
